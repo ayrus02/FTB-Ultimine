170b93e8a8f363557eead6ef16de0ead7a1935e4
ayrus02 <sly2000id@yahoo.com>
2024-07-10 19:00:00 +0700

    Up date Flat3x6Shape class to extends FlatShape class

--- common/src/main/java/dev/ftb/mods/ftbultimine/shape/Flat3x6Shape.java
+++ common/src/main/java/dev/ftb/mods/ftbultimine/shape/Flat3x6Shape.java
@@ -7,70 +7,15 @@ import net.minecraft.world.entity.player.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Flat3x6Shape implements Shape {
+public class Flat3x6Shape extends FlatShape {
     @Override
     public String getName() {
         return "flat_3x6";
     }
 
     @Override
-    public List<BlockPos> getBlocks(ShapeContext context) {
-        List<BlockPos> list = new ArrayList<>(18);
+    protected int maxWidth() { return 3; }
 
-        BlockPos basePos = context.pos();
-        list.add(basePos);
-        int depth = 0;
-
-        Player uncachedPlayer = FTBUltimine.instance.getUncachedPlayer();
-        String cardinalDirection = "";
-
-        if (uncachedPlayer != null)
-            cardinalDirection = FTBUltimine.instance.getCardinalDirection(uncachedPlayer.getDirection());
-
-        while (depth < maxDepth() && list.size() < context.maxBlocks()) {
-            int size = list.size();
-            LAYER: for (int a = -1; a <= 1; a++) {
-                    if (depth > 0 || a != 0) {
-
-                        BlockPos pos = basePos;
-
-                        switch (context.face().getAxis())
-                        {
-                            case X: // EAST(+) | WEST(-)
-                                pos = basePos.offset(0, 0, a);
-                                break;
-                            case Y: // UP(+) |  DOWN(-)
-                                if (cardinalDirection.equals("EAST")
-                                        || cardinalDirection.equals("WEST"))
-                                    pos = basePos.offset(0, 0, a);
-                                else // cardinalDirection = NORTH || SOUTH
-                                    pos = basePos.offset(a, 0, 0);
-                                break;
-                            case Z: // SOUTH(+) | NORTH(-)
-                                pos = basePos.offset(a, 0, 0);
-                                break;
-                        };
-
-                        if (context.check(pos)) {
-                            list.add(pos);
-
-                            if (list.size() >= context.maxBlocks()) {
-                                break LAYER;
-                            }
-                        }
-                    }
-            }
-            if (list.size() == size && depth > 0) {
-                break; // none of the blocks behind that could be mined: so stop the iteration
-            }
-            basePos = basePos.relative(context.face().getOpposite());
-            depth++;
-        }
-
-        return list;
-    }
-
-    protected int maxDepth() {
-        return 6;
-    }
+    @Override
+    protected int maxDepth() { return 6; }
 }
